e47beb78b036b7594b400962555c9ef74bec5956
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-09-23 22:08:31 -0500

    Dejando las bases listas

---
+++ app/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tienda en línea</title>
+  <link rel="stylesheet" href="assets/css/output.css">
+</head>
+<body class="bg-[url('../img/bg.png')]">
+  <div class="bg-white flex text-3xl items-center w-[]">
+
+    <div class="w-24 m-2 h-24 bg-[url('../img/logo.jpg')] bg-contain bg-no-repeat">
+      </div>
+  <p>
+
+  Tienda en línea
+
+  </p>
+    </div>
+</body>
+</html>
